d48f36c659e6c5778da560e9c79ee3d3ea6359a5
“ya903040” <asirov560@gmail.com>
2023-06-01 16:30:13 +0600

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,62 +1,21 @@
-let currentQuestion = 1;
+$(document).ready(function() {
+    $('#tree li').on('click',function() {
+        //style the selected answer
+        $(this).addClass('selectedAnswer').siblings("li").removeClass('selectedAnswer').addClass('notSelected');
+        $(this).find("li").addClass('selectedChild');
+        $(this).children("p.text").removeClass('hidden').addClass('show');
+        //show back btn
+        $(this).siblings(".back").removeClass('hidden').addClass('show');
+        $(this).parentsUntil("ul").find(".back").addClass('hidden').removeClass('show');
 
-function startTest() {
-    document.getElementById('startButton').style.display = "none";
-    showQuestion(currentQuestion);
-}
 
-function showQuestion(questionNumber) {
-    const questions = document.getElementsByClassName('question');
+    });
+//back
+    $('span.back').on('click',function() {
+        $(this).siblings("li").removeClass('notSelected selectedAnswer');
+        $(this).siblings("li").find("p.text").addClass('hidden');
+        //$(this).parentsUntil("ul").find("span.back").addClass('hidden');
+        $(this).addClass("hidden");
+    });
 
-    for (let i = 0; i < questions.length; i++) {
-        questions[i].style.display = "none";
-    }
-
-    if (questionNumber <= 5) {
-        document.getElementById('question' + questionNumber).style.display = "block";
-    }
-}
-
-function nextQuestion() {
-    currentQuestion++;
-    showQuestion(currentQuestion);
-}
-
-function calculateResult() {
-    const resultDiv = document.getElementById('result');
-    let result = "";
-
-    const answer1 = document.querySelector('input[name="q1"]:checked');
-    const answer2 = document.querySelector('input[name="q2"]:checked');
-    const answer3 = document.querySelector('input[name="q3"]:checked');
-    const answer4 = document.querySelector('input[name="q4"]:checked');
-    const answer5 = document.querySelector('input[name="q5"]:checked');
-
-    if (answer1 && answer2 && answer3 && answer4 && answer5) {
-        const developerType = answer1.value;
-        const dataInterest = answer2.value;
-        const designInterest = answer3.value;
-        const largeDataSets = answer4.value;
-        const problemSolving = answer5.value;
-
-        if (developerType === "frontend") {
-            if (designInterest === "yes") {
-                result = "You might relate to a Frontend Developer with a focus on UI/UX.";
-            } else {
-                result = "You might relate to a Frontend Developer with a focus on coding and functionality.";
-            }
-        } else {
-            if (dataInterest === "yes" && largeDataSets === "yes") {
-                result = "You might relate to a Data Engineer or Data Scientist.";
-            } else if (dataInterest === "yes" && largeDataSets === "no") {
-                result = "You might relate to a Backend Developer with a focus on databases and data manipulation.";
-            } else if (problemSolving === "yes") {
-                result = "You might relate to an Algorithm Engineer or Software Engineer.";
-            } else {
-                result = "You might relate to a Backend Developer with a focus on server-side programming and system architecture.";
-            }
-        }
-
-        resultDiv.innerHTML = "<p>" + result + "</p>";
-    }
-}
+});
